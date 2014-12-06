26e788e730b89d0171aa6ccab049b22c561d0bc1
Daniel Huhn <daniel_huhn@outlook.com>
2014-12-06 21:06:16 +0100

    replaced /sdcard with os call to get the directory

--- app/src/main/java/org/flipdot/flipdotapp/openDoor/OpenDoorConstants.java
+++ app/src/main/java/org/flipdot/flipdotapp/openDoor/OpenDoorConstants.java
@@ -1,9 +1,11 @@
 package org.flipdot.flipdotapp.openDoor;
 
+import android.os.Environment;
+
 /**
  * Created by daniel on 06.12.14.
  */
 public class OpenDoorConstants {
-    public static String PrivateKeyFilePath = "/sdcard/flipdot_doorsshkey";
+    public static String PrivateKeyFilePath = Environment.getExternalStorageDirectory().getPath() + "/flipdot_doorsshkey";
     public static String PublicKeyFilePath = PrivateKeyFilePath+".pub";
 }
