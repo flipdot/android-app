aec573cadcb70a5fc471024f57ec33943a2a4355
Daniel Huhn <daniel_huhn@outlook.com>
2014-12-28 17:34:54 +0100

    added new open space notification icons

--- app/src/main/java/org/flipdot/flipdotapp/notifications/SpaceOpenNotificationHandler.java
+++ app/src/main/java/org/flipdot/flipdotapp/notifications/SpaceOpenNotificationHandler.java
@@ -4,8 +4,11 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 
 import org.flipdot.flipdotapp.MainActivity;
 import org.flipdot.flipdotapp.R;
@@ -18,23 +21,40 @@ public class SpaceOpenNotificationHandler extends NotificationHandler {
     @Override
     public void HandleNotification(JSONObject parameters) {
         try {
-            String msg = parameters.getString("newStatus");
+            String newStatus = parameters.getString("newStatus");
+
+            int newStatusImage;
+            String header, message;
+            if(newStatus.equals("open")) {
+                newStatusImage = R.drawable.doorstatus_notification_open;
+                header = "hackerspace --> open";
+                message = "the hackerspace is now open";
+            } else if(newStatus.equals("close")) {
+                newStatusImage = R.drawable.doorstatus_notification_close;
+                header = "hackerspace --> closed";
+                message = "the hackerspace is now closed";
+            } else {
+                return;
+            }
+
+            Bitmap image = BitmapFactory.decodeResource(context.getResources(), newStatusImage);
 
             NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-            PendingIntent contentIntent = PendingIntent.getActivity(context, 0, new Intent(context, MainActivity.class), 0);
+            PendingIntent mainActivityIntent = PendingIntent.getActivity(context, 0, new Intent(context, MainActivity.class), 0);
 
-            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context);
-
-            notificationBuilder.setSmallIcon(R.drawable.doorstatus_open);
-            notificationBuilder.setContentTitle("New Notification!");
-            notificationBuilder.setStyle(new NotificationCompat.BigTextStyle().bigText(msg));
-            notificationBuilder.setContentIntent(contentIntent);
-            notificationBuilder.setVibrate(new long[]{100, 100, 100, 100});
-            notificationBuilder.setLights(Color.YELLOW, 500, 500);
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context)
+              .setSmallIcon(newStatusImage)
+              .setLargeIcon(image)
+              .setContentTitle(header)
+              .setStyle(new NotificationCompat.BigTextStyle().bigText(message))
+              .setContentIntent(mainActivityIntent)
+              .setVibrate(new long[]{100, 100, 100, 100})
+              .setLights(Color.YELLOW, 500, 500);
 
             final int NOTIFICATION_ID = 1;
             notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build());
         } catch (Exception ex){
+            Log.e("ERROR", ex.toString());
         }
     }
 }
